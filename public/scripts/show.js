c51808a3aa1f8ddd291cd2a891e32604373385dc
datagutt <datagutt666@hotmail.com>
2013-03-01 17:07:33 +0100

    Download support

---
+++ public/scripts/show.js
@@ -0,0 +1,29 @@
+(function(global){
+	if(jessie && jessie.isHostMethod && jessie.isHostMethod(Waffles, 'addToQueue')){
+		Waffles.addToQueue(function(){
+			if(jessie && jessie.areFeatures('query', 'forEach', 'attachListener', 'cancelDefault', 'createXhr', 'xhrSend')){
+				var show = window.location.pathname.split('/')[3];
+				var downloadButtons = jessie.query('.download');
+				jessie.forEach(downloadButtons, function(button){
+					jessie.attachListener(button, 'click', function download(e){
+						var season = button.getAttribute('data-season');
+						var episode = button.getAttribute('data-number');
+						jessie.xhrSend(jessie.createXhr(), '/api/download/' + show + '/' + season + '/' + episode, {
+							success: function(status){
+								if(status && status.responseText){
+									if(JSON && jessie.isHostMethod(JSON, 'parse')){
+										var data = JSON.parse(status.responseText);
+									}else{
+										var data = eval('(' + status.responseText + ')');
+									}
+									alert(data.message);
+								}
+							}
+						});
+						jessie.cancelDefault(e);
+					});
+				});
+			}
+		});
+	}
+})(window);
